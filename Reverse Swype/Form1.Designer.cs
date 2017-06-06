19ff29ca852917339e63cd779fbe9ca717293c66
AlfonsoVenancio <alfonso.venancio22@gmail.com>
2017-06-06 18:25:02 -0500

    Create dictionary
    Create the dictionary of the characters with the position of the center or the respective key in the keyboard, changed how the layout file needs to be read and first attempt to draw the line corresponding to the word

--- Reverse Swype/Form1.Designer.cs
+++ Reverse Swype/Form1.Designer.cs
@@ -33,13 +33,13 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.listKeyboards = new System.Windows.Forms.ComboBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.swypeImage = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.swypeImage)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -49,9 +49,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Palabra a obtener:";
+            this.label1.Text = "Word to translate:";
             // 
             // textBox1
             // 
@@ -65,9 +65,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(6, 57);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Teclado a ocupar:";
+            this.label2.Text = "Keyboard layout:";
             // 
             // listKeyboards
             // 
@@ -76,15 +76,16 @@
             this.listKeyboards.Name = "listKeyboards";
             this.listKeyboards.Size = new System.Drawing.Size(153, 21);
             this.listKeyboards.TabIndex = 3;
-            this.listKeyboards.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.listKeyboards.SelectedIndexChanged += new System.EventHandler(this.listKeyboards_SelectedIndexChanged);
             // 
-            // pictureBox1
+            // swypeImage
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 118);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(462, 190);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.swypeImage.Location = new System.Drawing.Point(12, 118);
+            this.swypeImage.Name = "swypeImage";
+            this.swypeImage.Size = new System.Drawing.Size(462, 190);
+            this.swypeImage.TabIndex = 4;
+            this.swypeImage.TabStop = false;
+            this.swypeImage.Paint += new System.Windows.Forms.PaintEventHandler(this.swypeImage_Paint);
             // 
             // groupBox1
             // 
@@ -97,7 +98,7 @@
             this.groupBox1.Size = new System.Drawing.Size(281, 100);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Datos de entrada";
+            this.groupBox1.Text = "Input";
             // 
             // groupBox2
             // 
@@ -108,16 +109,7 @@
             this.groupBox2.Size = new System.Drawing.Size(174, 74);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Datos de salida";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 29);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(147, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Nombre del archivo de salida:";
+            this.groupBox2.Text = "Output";
             // 
             // textBox2
             // 
@@ -126,14 +118,24 @@
             this.textBox2.Size = new System.Drawing.Size(144, 20);
             this.textBox2.TabIndex = 1;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Output file name:";
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(300, 91);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 23);
             this.button1.TabIndex = 7;
-            this.button1.Text = "Obtener Imagen";
+            this.button1.Text = "Get Image";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -143,12 +145,12 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.swypeImage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Reverse Swype";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.swypeImage)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -163,7 +165,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox listKeyboards;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox swypeImage;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox textBox2;
--- Reverse Swype/Form1.cs
+++ Reverse Swype/Form1.cs
@@ -12,22 +12,83 @@ namespace Reverse_Swype
 {
     public partial class Form1 : Form
     {
+        string directoryName;
+        Dictionary<char, Point> keyboardDictionary = new Dictionary<char, Point>();
+        int horizontalNumber=0, verticalNumber=0;
+        int heightChar = 0, widthChar = 0;
+        int verticalIdentation = 0, horizontalIdentation = 0;
+
+        private void swypeImage_Paint(object sender, PaintEventArgs e)
+        {
+            //Graphics strokeSwype = e.Graphics;
+            //Pen penSwype = new Pen(Color.AliceBlue);
+            //Brush brushSwype = new SolidBrush(Color.Red);
+            //strokeSwype.DrawLine(penSwype, 2, 2, 400, 450);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //swypeImage.Paint += new System.Windows.Forms.PaintEventHandler(this.swypeImage_Paint);
+            //this.Controls.Add(swypeImage);
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void listKeyboards_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            string layoutFileName = listKeyboards.SelectedItem.ToString();
+            string fullLayoutPath = directoryName+"\\"+layoutFileName+".txt";
+            string[] lines = System.IO.File.ReadAllLines(fullLayoutPath);
+            foreach (string line in lines)
+            {
+                horizontalNumber = Math.Max(line.Length, horizontalNumber);
+            }
+            verticalNumber = lines.Length;
+            heightChar = swypeImage.Height/verticalNumber;
+            widthChar = swypeImage.Width / horizontalNumber;
+            verticalIdentation = heightChar / 2;
+            horizontalIdentation = widthChar / 2;
+            int horizontalPosition = horizontalIdentation;
+            int verticalPosition = verticalIdentation;
+            foreach (string line in lines)
+            {
+                horizontalPosition = horizontalIdentation;
+                //Console.WriteLine(verticalPosition.ToString());
+                foreach (char character in line)
+                {
+                    if (character == '_')
+                    {
+                        horizontalPosition -= horizontalIdentation;
+                    }
+                    else
+                    {                    
+                        if (character != '$')
+                        {
+                            keyboardDictionary[character] = new Point(horizontalPosition,verticalPosition);
+                        }
+                        else
+                        {
+                            horizontalPosition += horizontalIdentation;
+                        }
+                    }
+                    horizontalPosition += widthChar;
+                }
+                verticalPosition += heightChar;
+            }
+            //foreach(KeyValuePair<char,Point> dictionaryEntry in keyboardDictionary)
+            //{
+            //    Console.WriteLine("Key = {0}, Value = {1}", dictionaryEntry.Key.ToString(), dictionaryEntry.Value.ToString());
+            //}
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            //System.IO.Path.GetDirectoryName(Application.ExecutablePath);
-            
-            string directoryName = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(Application.ExecutablePath)))+"\\layouts";
-            Console.WriteLine(directoryName);
+        {           
+            directoryName = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(Application.ExecutablePath)))+"\\layouts";
+            //Console.WriteLine(directoryName);
             string[] fileNames = System.IO.Directory.GetFiles(directoryName);
             List<string> listFileNames = new List<string>();
             foreach (string fileName in fileNames)
@@ -35,9 +96,8 @@ namespace Reverse_Swype
                 listFileNames.Add(fileName.Replace(directoryName+"\\", "").Replace(".txt",""));
             }
             string[] fileNamesClear = listFileNames.ToArray();
-            //this.comboBox1.Items.AddRange(files);
             this.listKeyboards.Items.AddRange(fileNamesClear);
-            listKeyboards.SelectedItem = 1;
+            listKeyboards.SelectedItem = 0;
         }
     }
 }
